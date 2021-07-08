6917396ec0eea8777287e8c9485eebdce14b281e
Stephen <step.twig@gmail.com>
2021-07-09 00:46:31 +0100

    add bootstrap css and js

---
+++ includes/footerJS.php
@@ -0,0 +1,18 @@
+<!-- <script src="https://code.jquery.com/jquery-3.3.1.js"></script> -->
+<!-- <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script> -->
+<!-- <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script> -->
+
+<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.js"><\/script>')</script>
+<script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+<script src="js/dataTables.bootstrap.min.js"></script>
+
+<script src="js/dataTables.buttons.min.js"></script>
+<script src="js/buttons.print.min.js"></script>
+<script src="js/vfs_fonts.js"></script>
+<script src="js/pdfmake.min.js"></script>
+<script src="js/jszip.min.js"></script>
+<script src="js/buttons.html5.min.js"></script>
+<script src="js/buttons.flash.min.js"></script>
+<script src="js/sum().js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-select.js"></script>
---
+++ includes/headerCSS.php
@@ -0,0 +1,12 @@
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+   <!-- Bootstrap -->
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/buttons.dataTables.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/adminlte.min.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.css">
+    <link href="css/custom.css" rel="stylesheet">
+    <script src="js/respond.js"></script>

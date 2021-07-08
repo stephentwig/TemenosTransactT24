a602e44ea5895f5d89b1d2d46c96bf6d62e92463
Stephen <step.twig@gmail.com>
2021-07-09 00:39:15 +0100

    Initial Commit - Joy to the world

---
+++ getDetails.php
@@ -0,0 +1,176 @@
+<?php 
+
+
+// get T24 server IP and port number
+$IP = $_POST["IP_address"];
+$PORT = $_POST["Port_number"];
+
+//get request values
+$T24_Application = $_POST["Application"];
+$T24_VersionName = $_POST["Version"];
+$T24_Function = $_POST["Function"];// S, I ,D, H , V ,R - can't do C -COpy and P - PRint ? or PASTE
+$T24_ProcessType = $_POST["ProcessType"];// OR VALIDATE
+$T24_GTS_CONTROL =  $_POST["GTS_Control"];  // blank or null ,1,2,3,4  >> FOR error handling either reject errors and accept overrides // or store in NAU that is IHLD
+$T24_No_of_Authorisers =  $_POST["No_of_Auth"];
+$T24_OFSAction = $_POST["OFSAction"]; // For AA or one [1] for REPLACEMENT or UPDATING an existing Record
+$T24_username = $_POST["Username"];
+$T24_password = $_POST["Password"];
+$T24_CompanyCode = $_POST["CompanyCode"]; //GH0010001
+$T24_TransactionID = $_POST["T24_TransactionID"];
+$T24_MessageID = $_POST["MessageID"];
+$T24_MessageDATA =  $_POST["MessageDATA"];
+
+//store 3rd party generated ID in a local ref
+
+$OFS_Transaction_REQUEST_STRING = "$T24_Application,$T24_VersionName/$T24_Function/$T24_ProcessType/$T24_GTS_CONTROL/$T24_No_of_Authorisers/$T24_OFSAction,$T24_username/$T24_password/$T24_CompanyCode,$T24_TransactionID/$T24_MessageID,$T24_MessageDATA";
+
+
+// raw OFS request
+echo "Raw OFS request :".$OFS_Transaction_REQUEST_STRING;
+echo "<br>";
+
+//encode spaces in the strings to be sent 
+$OFS_Transaction_REQUEST_STRING = urlencode($OFS_Transaction_REQUEST_STRING);
+
+
+echo "<br>";
+echo $OFS_Transaction_REQUEST_STRING;
+
+echo "<br>";
+
+$T24_OFS_BROWSER_URL = "http://$IP:$PORT/TAFJServices/services/OFSService/Invoke?Request=$OFS_Transaction_REQUEST_STRING";
+
+$response = file_get_contents("$T24_OFS_BROWSER_URL");	
+
+
+// example of OFS string
+//CUSTOMER,CUSTOMERVERSION/S/PROCESS/1/2,INPUTT/12345/GH01001,TRANSACTION_ID
+echo "Request: ".$T24_OFS_BROWSER_URL;
+
+
+// echo "Response: ".$response;
+echo "<br>";
+
+$FormattedResponse = explode("=", $response);
+
+
+//commas in the field values has to be handled eg. ADDRESS:1:1=ACCRA,GHANA,NAME:1:1=MEREDITH
+// print_r($FormattedResponse);
+echo "<br>";
+
+
+
+
+$arraylength = count($FormattedResponse) ;
+$theFields = array();
+$theValues = array();
+
+
+
+echo "<br>";
+
+// loop through the formatted response
+for ($i=3; $i < $arraylength; $i++) { 
+		
+		//$FormattedResponse[$i];
+		//find the last occurence of the ","
+		$commaPosition = strrpos($FormattedResponse[$i], ",");
+
+		//then split the array into two
+
+		$valueResponse = substr($FormattedResponse[$i], 0, $commaPosition);
+		//$valueResponse = $field_value[0];
+		$theValues[] = $valueResponse;
+
+		
+		//get the starting point of the comma position by StringLength - Comma Position and read from the back
+		$commaPosition = strlen($FormattedResponse[$i]) - $commaPosition - 1;
+
+		//to get the Field , move one step forware (+1) to skip the ","
+		$FieldResponse = substr($FormattedResponse[$i],  -1 * ($commaPosition) );
+
+		//convert the : to _ in the fields
+		$FieldResponse = str_replace(":", "_", $FieldResponse);
+
+		//finally convert the . to double underscore __ in the fields
+		$FieldResponse = str_replace(".", "__", $FieldResponse);
+
+		// echo $FieldResponse = $field_value[1];
+		$theFields[] = $FieldResponse;
+
+		
+		//to get the last value element
+		//theres a default 0 response attached to the last value element
+		//remove it
+		//because we start the loop from the 3rd index to get the last index subtract 4 from arraylength
+		if ($i == ($arraylength - 1)) {
+			//theres a default 0 response attached to the last value element
+			//remove it
+			$theValues[] = $FormattedResponse[$arraylength - 1];
+		}
+
+
+}
+
+
+
+// now update the associative array
+//remove the xml in response values
+
+echo "<br>";
+// echo "The values 70:".$theValues[-4];
+echo "<br>";
+// remove the last element
+$theFields = array_splice($theFields, 0, -1);
+// print_r($theFields);
+
+//start from array index 1
+// $newFields = array_splice($theFields, 1);
+
+echo "<br>";
+
+// print_r($theValues);
+echo "<br>";
+// echo "<br> VALUES >>";
+//start from array index 1
+$newValues = array_splice($theValues, 1);
+//replace the last element with the last but 2 element 
+$newValues[count($newValues) - 2] = $newValues[count($newValues) - 1];
+//REMOVE the last element 
+// To-do 69 characters of xml trailing
+$newValues = array_splice($newValues, 0, -1);
+// print_r($newValues);
+
+//associative array for the field and values
+$field_value_pairs = array();
+
+//then populate an associative array with the FIELD or KEY - VALUE pairs
+$theFields_count = count($theFields);
+// echo "the fields $theFields_count";
+
+for ($i=0; $i < $theFields_count  ; $i++) { 
+	
+	//eg (MNEMONIC_1_1 => SSARPONG1)
+	$field_value_pairs[$theFields[$i]] = $newValues[$i];
+
+}
+
+// print_r($field_value_pairs);
+
+
+
+echo "this is ADDRESS name ".$field_value_pairs["ADDRESS_1_1"];
+echo "<br>";
+echo "this is EMAIL ADDRESS:".$field_value_pairs["EMPL__EMAIL_1_1"];
+echo "<br>";
+echo $field_value_pairs["NAME__1_1_1"]." is from ".$field_value_pairs["TOWN__COUNTRY_1_1"];
+
+
+echo "<br>";
+
+$T24_response = json_encode($field_value_pairs);
+
+echo $T24_response;
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,115 @@
+<div class="container">
+		 <form method="POST" action="getDetails.php">
+
+					 	<div>
+					 		<label>T24 Web Server IP: </label>
+					 		<input type="" name="IP_address" value="localhost">
+					 	</div>
+					 	
+					 	<div>
+					 		<label>T24 Web Server Port: </label>
+					 		<input type="" name="Port_number" value="8085">
+					 	</div>
+						
+							<hr>
+
+
+					 	<div>
+					 		<label>Application: </label>
+					 		<input type="" name="Application">
+					 	</div>
+					 	
+					 	<div>
+					 		<label>Version: </label>
+					 		<input type="" name="Version">
+					 	</div>
+
+					 	<div>
+					 		<label>Function: </label>
+					 		<input type="" name="Function">
+					 	</div>
+
+					 	<div>
+					 		<label>Process Type: </label>
+					 		<input type="" name="ProcessType" value="PROCESS">
+					 	</div>
+						
+					 	<div>
+					 		GTS Control : 
+					 		<input type="" name="T24_TransactionID">
+					 	</div>
+						
+						<div>
+					 		<label>Number of Authorisers: </label>
+					 		<input type="" name="No_of_Auth">
+					 	</div>
+					 	
+					 	<div>
+					 		<label>OFS Action: </label>
+					 		<input type="" name="OFSAction">
+					 	</div>
+						
+
+						<hr>
+					 	
+
+
+					 	<div>
+					 		<label>Username: </label>
+					 		<input type="" name="Username" value="INPUTT">
+					 	</div>
+
+					 	<div>
+					 		<label>Password: </label>
+					 		<input type="password" name="Password" value="123456">
+					 	</div>
+						
+
+						<hr>
+
+					 	<div>
+					 		 <label>Company Code: </label> 
+					 		 <input type="" name="CompanyCode">
+					 	</div>
+
+					 	<div>
+					 		 <label>T24 Record ID: </label> 
+					 		 <input type="" name="T24_TransactionID">
+					 	</div>
+
+					 	<div>
+					 		<label>Message ID</label> 
+					 		<input type="" name="MessageID">
+					 	</div>
+
+					 	<div>
+					 		<label>Message Data</label>
+					 		<textarea name="MessageDATA"></textarea>
+					 	</div>
+						
+						<input type="submit" name="submit" value="Submit to Temenos Transact (T24)">
+
+		 </form>
+
+
+
+		<br>
+		<pre>
+
+		$T24_Application = "CUSTOMER";
+		$T24_VersionName = "";
+		$T24_Function = "S"; // S, I ,D, H , V ,R - can't do C -COpy and P - PRint ? or PASTE
+		$T24_ProcessType = "PROCESS"; // OR VALIDATE
+		$T24_GTS_CONTROL = "1";  // blank or null ,1,2,3,4  >> FOR error handling either reject errors and accept overrides // or store in NAU that is IHLD
+		$T24_No_of_Authorisers = "0";
+		$T24_OFSAction = "1"; // For AA or one [1] for REPLACEMENT
+		$T24_username = "INPUTT";
+		$T24_password = "123456";
+		$T24_CompanyCode = ""; //GH0010001
+		$T24_TransactionID = "190097";
+		$T24_MessageID = "";
+		$T24_MessageDATA = "MNEMONIC:1:1=TONY99,SHORT.NAME:1:1=TONY ,SHORT.NAME:2:1=ANTHONY,NAME.1:1:1=Tony Stark,NAME.2:1:1=Tony Stark,STREET:1:1=OXFORD II STREER,STREET:2:1=WALL STREET,ADDRESS:1:1=ADUM NEYORK,ADDRESS:1:2=DOMEe,ADDRESS:2:1=ADiM 2,ADDRESS:3:1=ADUM 3,SECTOR:1:1=1100,ACCOUNT.OFFICER:1:1=1,INDUSTRY:1:1=1007,TARGET:1:1=1,NATIONALITY:1:1=AG,CUSTOMER.STATUS:1:1=1,RESIDENCE:1:1=GH,LANGUAGE:1:1=1,TITLE:1:1=Alhaji,GIVEN.NAMES:1:1=Tony,FAMILY.NAME:1:1=Stark,GENDER:1:1=MALE";
+
+</pre>
+
+</div>

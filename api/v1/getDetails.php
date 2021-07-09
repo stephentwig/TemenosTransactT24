ecbceb39a379de4bc2888af63f6ad73297ee0e4d
Stephen <step.twig@gmail.com>
2021-07-09 02:52:01 +0100

    Update getDetails.php

--- api/v1/getDetails.php
+++ api/v1/getDetails.php
@@ -1,8 +1,8 @@
 <?php 
-header("Content-Type:application/json");
+// header("Content-Type:application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-
+header("Access-Control-Allow-Methods: GET, POST ,OPTIONS");
 
 // get T24 server IP and port number
 $IP = $_POST["IP_address"];
@@ -27,19 +27,22 @@ $T24_MessageDATA =  $_POST["MessageDATA"];
 
 $OFS_Transaction_REQUEST_STRING = "$T24_Application,$T24_VersionName/$T24_Function/$T24_ProcessType/$T24_GTS_CONTROL/$T24_No_of_Authorisers/$T24_OFSAction,$T24_username/$T24_password/$T24_CompanyCode,$T24_TransactionID/$T24_MessageID,$T24_MessageDATA";
 
+//associative array for the field and values
+$field_value_pairs = array();
 
 // raw OFS request
-echo "Raw OFS request :".$OFS_Transaction_REQUEST_STRING;
-echo "<br>";
+// echo "Raw OFS request :".$OFS_Transaction_REQUEST_STRING;
+
+
+//push a ofsRequest to the array
+$field_value_pairs["ofsRequest"] = $OFS_Transaction_REQUEST_STRING;
+
 
 //encode spaces in the strings to be sent 
 $OFS_Transaction_REQUEST_STRING = urlencode($OFS_Transaction_REQUEST_STRING);
 
 
-echo "<br>";
-echo $OFS_Transaction_REQUEST_STRING;
 
-echo "<br>";
 
 $T24_OFS_BROWSER_URL = "http://$IP:$PORT/TAFJServices/services/OFSService/Invoke?Request=$OFS_Transaction_REQUEST_STRING";
 
@@ -48,29 +51,26 @@ $response = file_get_contents("$T24_OFS_BROWSER_URL");
 
 // example of OFS string
 //CUSTOMER,CUSTOMERVERSION/S/PROCESS/1/2,INPUTT/12345/GH01001,TRANSACTION_ID
-echo "Request: ".$T24_OFS_BROWSER_URL;
+// echo "Request: ".$T24_OFS_BROWSER_URL;
 
 
-// echo "Response: ".$response;
-echo "<br>";
+echo "Response: ".$response;
+
 
 $FormattedResponse = explode("=", $response);
 
 
 //commas in the field values has to be handled eg. ADDRESS:1:1=ACCRA,GHANA,NAME:1:1=MEREDITH
-// print_r($FormattedResponse);
-echo "<br>";
 
 
-
-
+//to get the number of elements in array
 $arraylength = count($FormattedResponse) ;
 $theFields = array();
 $theValues = array();
 
 
 
-echo "<br>";
+
 
 // loop through the formatted response
 for ($i=3; $i < $arraylength; $i++) { 
@@ -120,9 +120,7 @@ for ($i=3; $i < $arraylength; $i++) {
 // now update the associative array
 //remove the xml in response values
 
-echo "<br>";
-// echo "The values 70:".$theValues[-4];
-echo "<br>";
+
 // remove the last element
 $theFields = array_splice($theFields, 0, -1);
 // print_r($theFields);
@@ -130,11 +128,7 @@ $theFields = array_splice($theFields, 0, -1);
 //start from array index 1
 // $newFields = array_splice($theFields, 1);
 
-echo "<br>";
 
-// print_r($theValues);
-echo "<br>";
-// echo "<br> VALUES >>";
 //start from array index 1
 $newValues = array_splice($theValues, 1);
 //replace the last element with the last but 2 element 
@@ -143,9 +137,6 @@ $newValues[count($newValues) - 2] = $newValues[count($newValues) - 1];
 // To-do 69 characters of xml trailing
 $newValues = array_splice($newValues, 0, -1);
 // print_r($newValues);
-
-//associative array for the field and values
-$field_value_pairs = array();
 
 //then populate an associative array with the FIELD or KEY - VALUE pairs
 $theFields_count = count($theFields);
@@ -160,10 +151,9 @@ for ($i=0; $i < $theFields_count  ; $i++) {
 
 
 
-
 $T24_response = json_encode($field_value_pairs);
 
-echo "<pre>".$T24_response."</pre>";
+echo $T24_response;
 
 
  ?>
